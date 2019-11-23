09cb47116e73684475e89508f9ed4300f266f6ee
Pim van Schayik <pimvschayik@live.nl>
2019-11-23 15:11:59 +0000

    feat: home header

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const { src, dest, series, parallel, watch } = require('gulp')
 const fileinclude = require('gulp-file-include')
-// var watch = require('gulp-watch')
+const htmlbeautify = require('gulp-html-beautify')
 var stylus = require('gulp-stylus')
 var browserSync = require('browser-sync')
 
@@ -26,11 +26,16 @@ function html() {
       prefix: '@@',
       basepath: '@file'
     }))
+    .pipe(htmlbeautify({ "indent_size": 2, "preserve_newlines": false, "end_with_newline": true, "jslint_happy": true }))
     .pipe(dest('./public'));
 }
 
 function watchHtml() {
   return watch('./html/**', series(html, reload));
+}
+
+function watchJson() {
+  return watch('./data/**', series(html, reload));
 }
 
 function css() {
@@ -40,12 +45,8 @@ function css() {
 }
 
 function watchCss() {
-  return watch('./stylus/*.styl', series(css, reload));
+  return watch('./stylus/**', series(css, reload));
 }
-
-const compile = series(html, css);
-const watchAll = parallel(watchHtml, watchCss)
-const dev = series(compile, serve, watchAll);
 
 exports.serve = serve;
 exports.reload = reload;
@@ -56,7 +57,12 @@ exports.watchHtml = watchHtml;
 exports.css = css;
 exports.watchCss = watchCss;
 
+const compile = series(html, css);
+const watchAll = parallel(watchHtml, watchCss, watchJson)
+const dev = series(compile, serve, watchAll);
+
 exports.compile = compile;
+exports.watchAll = watchAll;
 exports.dev = dev;
 
-exports.default = parallel(serve, reload, html, watchHtml, css, watchCss, compile, dev);
+exports.default = dev
