ea21c39662be1f69a8b588b8bba41b7656f096ee
Pim van Schayik <pimvschayik@live.nl>
2019-12-12 14:10:00 +0000

    del: extra files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
 const { src, dest, series, parallel, watch } = require('gulp')
-// const fileinclude = require('gulp-file-include')
 const htmlbeautify = require('gulp-html-beautify')
-const markdown = require('markdown');
-var stylus = require('gulp-stylus')
-var browserSync = require('browser-sync')
-var nunjucksRender = require('gulp-nunjucks-render');
-
+const stylus = require('gulp-stylus')
+const browserSync = require('browser-sync')
+const nunjucksRender = require('gulp-nunjucks-render');
 
 const server = browserSync.create();
 
@@ -25,30 +22,20 @@ function reload(done) {
 
 function html() {
   return src(['./nunjucks/*.njk'])
-    // .pipe(fileinclude({
-    //   prefix: '@@',
-    //   basepath: '@file',
-    //   filters: {
-    //     markdown: markdown.parse
-    //   }
-    // }))
     .pipe(nunjucksRender({
       path: ['nunjucks']
     }))
-    .pipe(htmlbeautify({ "indent_size": 2, "preserve_newlines": false, "end_with_newline": true, "jslint_happy": true }))
+    .pipe(htmlbeautify({ 
+      "indent_size": 2, 
+      "preserve_newlines": false, 
+      "end_with_newline": true, 
+      "jslint_happy": true 
+    }))
     .pipe(dest('./public'));
 }
 
 function watchHtml() {
   return watch('./nunjucks/**', series(html, reload));
-}
-
-function watchJson() {
-  return watch('./data/**', series(html, reload));
-}
-
-function watchMarkdown() {
-  return watch('./markdown/**', series(html, reload));
 }
 
 function css() {
@@ -71,7 +58,7 @@ exports.css = css;
 exports.watchCss = watchCss;
 
 const compile = series(html, css);
-const watchAll = parallel(watchHtml, watchCss, watchJson, watchMarkdown)
+const watchAll = parallel(watchHtml, watchCss)
 const dev = series(compile, serve, watchAll);
 
 exports.compile = compile;
